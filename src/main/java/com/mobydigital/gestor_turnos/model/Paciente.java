45aaa38fe5df88ac3c637952fdb250e36a9c91eb
cbraian42 <127146446+cbraian42@users.noreply.github.com>
2026-05-04 14:56:28 -0300

    feat(paciente): crea entidad Paciente

---
+++ src/main/java/com/mobydigital/gestor_turnos/model/Paciente.java
@@ -0,0 +1,30 @@
+package com.mobydigital.gestor_turnos.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "pacientes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false, unique = true)
+    private String dni;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}

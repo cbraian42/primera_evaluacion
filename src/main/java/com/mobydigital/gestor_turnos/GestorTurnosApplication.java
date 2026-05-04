5353f39ed1f9da0ca79af59034577bba23a9c800
cbraian42 <127146446+cbraian42@users.noreply.github.com>
2026-05-04 14:29:29 -0300

    chore: inicializar proyecto base con dependencias

---
+++ src/main/java/com/mobydigital/gestor_turnos/GestorTurnosApplication.java
@@ -0,0 +1,13 @@
+package com.mobydigital.gestor_turnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorTurnosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobydigital/gestor_turnos/GestorTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobydigital.gestor_turnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

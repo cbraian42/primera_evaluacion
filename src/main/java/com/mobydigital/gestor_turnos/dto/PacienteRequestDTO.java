51b8bce50ce8c79d6965fa6d28a4d7d9d235305a
cbraian42 <127146446+cbraian42@users.noreply.github.com>
2026-05-04 22:30:37 -0300

    feat(paciente): crea request DTO

---
+++ src/main/java/com/mobydigital/gestor_turnos/dto/PacienteRequestDTO.java
@@ -0,0 +1,28 @@
+package com.mobydigital.gestor_turnos.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PacienteRequestDTO {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
+    private String apellido;
+
+    @NotBlank(message = "El DNI es obligatorio")
+    private String dni;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ser una dirección de email válida")
+    private String email;
+}
